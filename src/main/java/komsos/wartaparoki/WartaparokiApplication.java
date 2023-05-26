7b34571798852f013ac93cc42f75ead7ac9441fe
Anggun Dwi Cahyadi <mr.octavianz@gmail.com>
2023-05-26 12:11:00 +0700

    initial commit

---
+++ src/main/java/komsos/wartaparoki/WartaparokiApplication.java
@@ -0,0 +1,13 @@
+package komsos.wartaparoki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WartaparokiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WartaparokiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/komsos/wartaparoki/WartaparokiApplicationTests.java
@@ -0,0 +1,13 @@
+package komsos.wartaparoki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WartaparokiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
